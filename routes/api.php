1163eab766824ff3b5f19782558ab70b41a65728
juliogabrielxp <p90degold@gmail.com>
2026-09-14 20:22:33 -0300

    Recria rotas de API completas com grupo auth:sanctum e correção de segurança no AgendamentoController

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AgendamentoController;
+use App\Http\Controllers\Auth\MeController;
+use App\Http\Controllers\ServicoController;
+use App\Http\Controllers\ProfissionalController;
+use App\Http\Controllers\AgendaController;
+use App\Http\Controllers\BarbeariaPublicaController;
+use App\Http\Controllers\MeusAgendamentosController;
 
-Route::post('/agendamentos', [AgendamentoController::class, 'store']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [MeController::class, 'show']);
+
+    Route::apiResource('servicos', ServicoController::class)->except(['show']);
+    Route::apiResource('profissionais', ProfissionalController::class)->except(['show']);
+
+    Route::get('/agenda', [AgendaController::class, 'index']);
+    Route::get('/barbearias', [BarbeariaPublicaController::class, 'index']);
+    Route::get('/barbearias/{id}', [BarbeariaPublicaController::class, 'show']);
+    Route::get('/meus-agendamentos', [MeusAgendamentosController::class, 'index']);
+
+    Route::post('/agendamentos', [AgendamentoController::class, 'store']);
+});
